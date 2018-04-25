633af36d6421cb98470f65fca23075e86d0470b7
Ryan Manro <rmanro@gmail.com>
2018-04-25 10:43:20 -0700

    further SIGNIN tests OK

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -34,6 +34,44 @@ describe.only('Auth API', () => {
             .then(({ body }) => {
                 assert.ok(body.token);
             });
+    });
 
+    it('Gives 400 on Signup of Same Email', () => {
+        return request
+            .post('/auth/signup')
+            .send({
+                email: 'me@me.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Email Exists');
+            });
+    });
+
+    it('Gives 401 on Non-Existent Email', () => {
+        return request
+            .post('/auth/signin')
+            .send({
+                email: 'bad@me.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid Email or Password');
+            });
+    });
+
+    it('Gives 401 on Bad Password', () => {
+        return request
+            .post('/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: 'abd'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid Email or Password');
+            });
     });
 });
