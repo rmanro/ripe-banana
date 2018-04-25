479d6726fedc1ce3399db54b2567ef4c650c1ed3
Ryan Manro <rmanro@gmail.com>
2018-04-25 10:15:22 -0700

    signup testing OK

---
+++ test/e2e/auth.test.js
@@ -0,0 +1,27 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+describe.only('Auth API', () => {
+
+    beforeEach(() => dropCollection('reviewers'));
+
+    let token = null;
+
+    beforeEach(() => {
+        return request 
+            .post('/auth/signup')
+            .send({
+                email: 'me@me.com',
+                password: 'abc',
+                name: 'Roger Ebert',
+                company: 'Chicago Sun-Times'
+            })
+            .then(({ body }) => token = body.token);
+    });
+
+    it('Signup', () => {
+        assert.ok(token);
+    });
+
+});
